7e60005e6944cfc5dceb33b5cabdee1aea88e115
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-18 15:05:23 -0500

    app built without testing added

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Display from './components/display'
 import Dashboard from './components/dashboard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+
+      <Dashboard />
     </div>
   );
 }
