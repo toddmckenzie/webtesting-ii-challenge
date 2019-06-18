306d1229226134d6fabc2cb56745126edc252aa3
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-06-18 14:32:34 -0500

    basic react setup git commi

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from './components/display'
+import Dashboard from './components/dashboard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
